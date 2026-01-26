0403f7ce661f605e09b704150fb0dcc4fd8343b8
yan4093L <cyly1002@gmail.com>
2026-01-26 11:53:17 +0100

    feat: implement variables and secrets

--- variables.tf
+++ variables.tf
@@ -2,10 +2,12 @@ variable "minio_server" {
   type    = string
   default = "localhost:9000"
 }
+
 variable "minio_user" {
   type    = string
   default = "minioadmin"
 }
+
 variable "minio_password" {
   type      = string
   sensitive = true
