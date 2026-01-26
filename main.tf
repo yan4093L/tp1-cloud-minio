d5fd98120de0285d431b91aed2264830fe02399e
yan4093L <cyly1002@gmail.com>
2026-01-26 11:38:50 +0100

    feat: parameterize credentials using variables

--- main.tf
+++ main.tf
@@ -8,13 +8,34 @@ terraform {
 }
 
 provider "minio" {
-  minio_server     = "localhost:9000"
-  minio_access_key = "minioadmin"
-  minio_secret_key = "minioadmin"
-  minio_ssl        = false
+  minio_server   = var.minio_server
+  minio_user     = var.minio_user
+  minio_password = var.minio_password
+  minio_ssl      = false
 }
 
 resource "minio_s3_bucket" "tp1_bucket" {
   bucket = "tp1-cloud-bucket"
   acl    = "private"
+}
+
+resource "minio_s3_bucket" "web_bucket" {
+  bucket = "webbucket"
+  acl    = "private"
+}
+
+resource "minio_s3_object" "index_html" {
+  bucket_name  = minio_s3_bucket.web_bucket.bucket
+  object_name  = "index.html"
+  source       = "index.html"
+  content_type = "text/html"
+  acl          = "public-read"
+}
+
+resource "minio_s3_object" "style_css" {
+  bucket_name  = minio_s3_bucket.web_bucket.bucket
+  object_name  = "style.css"
+  source       = "style.css"
+  content_type = "text/css"
+  acl          = "public-read"
 }
---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "minio_server" {
+  type    = string
+  default = "localhost:9000"
+}
+variable "minio_user" {
+  type    = string
+  default = "minioadmin"
+}
+variable "minio_password" {
+  type      = string
+  sensitive = true
+  default   = "minioadmin"
+}
